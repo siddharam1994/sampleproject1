91358731d7aef11de324a6d878abdd675421c47a
Siddharam1994 <valsangesid4785@gmail.com>
2023-01-28 21:07:33 +0530

    addingClass

---
+++ src/main/java/PrintClass.java
@@ -0,0 +1,22 @@
+public class PrintClass {
+
+public void printingLine(){
+    System.out.println("Sid Coding");
+
+}
+
+public static void main (String[] args){
+    PrintClass object = new PrintClass();
+    object.printingLine();
+
+
+}
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/test1.feature
@@ -0,0 +1,6 @@
+Feature: Calculation methods
+
+  Scenario: Addition operators
+* def readingClass = Java.type('PrintClass')
+    * def object = PrintClass.printingLine
+    * print object
